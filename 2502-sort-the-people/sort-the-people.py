51068e366f8fbfcb691c69501bf9e1c634eb31fa
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-08-09 16:47:47 -0400

    Time: 102 ms (61.88%) | Memory: 17 MB (91.86%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        '''m_dict={}
+        for i in range(len(names)):
+            m_dict[heights[i]]=names[i]
+        for i in range(len(heights)):
+            for j in range(i+1, len(heights)):
+                if heights[i]<heights[j]:
+                    heights[i],heights[j]=heights[j],heights[i]
+        names.clear()
+        for i in heights:
+            names.append(m_dict[i])
+        return names'''
+
+        ## Other way of solving the problem:
+        res_dict = {}
+
+        for i in range(len(heights)):
+            res_dict[heights[i]] = names[i]
+        sorted_keys = sorted(res_dict.keys(), reverse = True)
+        return [res_dict[key] for key in sorted_keys]
+
+
+
+
+
+        for i in range(len(heights)):
+            for j in range(i+1, len(heights)):
+                if heights[i]<heights[j]:
+                    heights[i],heights[j]=heights[j],heights[i]
+                    names[i],names[j]=names[j],names[i]
+        return names
