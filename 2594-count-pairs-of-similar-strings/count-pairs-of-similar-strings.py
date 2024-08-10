90c75dbe040388d91770518e545dfe0813ae26f1
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-08-09 23:18:28 -0400

    Time: 52 ms (95.93%) | Memory: 16.7 MB (48.87%) - LeetSync

---
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+        # count=0
+        # for i in range(len(words)):
+        #     for j in range(i+1, len(words)):
+        #         if set(words[i]) == set(words[j]):
+        #             count+=1
+        # return count
+
+        # SECOND ATTEMPT USING HASH MAP, SETS AND PAIRS FROM A SET
+        temp_dict = {}
+        for word in words:
+            temp = "".join(sorted(set(word)))
+            if temp not in temp_dict:
+                temp_dict[temp] = 1
+            else:
+                temp_dict[temp] += 1
+        count = 0
+        for value in temp_dict.values():
+            count += value * (value-1)/2
+        return int(count)
