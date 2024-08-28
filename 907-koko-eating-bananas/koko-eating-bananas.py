e5b09b86d24a53e6474aaaba45bbf16d409b13b3
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-08-28 13:37:42 -0400

    Time: 267 ms (53.65%) | Memory: 18.1 MB (56.40%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -3,22 +3,41 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         # Using binary search to find the min number for which all bananas would be finished before the available hours
 
+        # l = 1
+        # r = max(piles)
+        # res = r
+
+        # while l<=r:
+        #     k = (l+r)//2
+        #     hours = 0
+        #     for p in piles:
+        #         hours += math.ceil(p/k)
+
+        #     if hours <= h:
+        #         res= min(res, k)
+        #         r = k-1
+        #     else: 
+        #         l = k+1
+        # return res
+ 
+# SECOND ATTEMPT
+# Try to create a function for condition and convert the problem into a simple binary search problem
+
+        def condition (self, speed):
+            if sum(ceil(pile/speed) for pile in piles) <= h:
+                return True
+            return False
+        # Binary Search Simple Implementation
+
         l = 1
         r = max(piles)
-        res = r
 
-        while l<=r:
-            k = (l+r)//2
-            hours = 0
-            for p in piles:
-                hours += math.ceil(p/k)
+        while l < r:
+            mid = l + (r-l)//2
 
-            if hours <= h:
-                res= min(res, k)
-                r = k-1
-            else: 
-                l = k+1
-        return res
- 
-
+            if condition(self, mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
         
