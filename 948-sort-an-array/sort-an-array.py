996609ce9a5f27d7c339c1fa63fd00f3001ac2ba
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-09-15 14:33:40 -0400

    Time: 1131 ms (39.72%) | Memory: 23.9 MB (82.66%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,78 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        # if len(nums) == 1:
+        #     return nums
+        # if len(nums) > 1:
+        #     mid = len(nums)//2
+
+        #     left = nums[:mid]
+        #     right = nums[mid:]
+
+        #     self.sortArray(left)
+        #     self.sortArray(right)
+
+        #     i=j=k=0
+        #     while i < len(left) and j < len(right):
+        #         if left[i] < right[j]:
+        #             nums[k] = left[i]
+        #             i += 1
+        #         else:
+        #             nums[k] = right[j]
+        #             j += 1
+        #         k += 1
+        #     while i < len(left):
+        #         nums[k] = left[i]
+        #         i += 1
+        #         k += 1
+        #     while j < len(right):
+        #         nums[k] = right[j]
+        #         j += 1
+        #         k += 1
+        #     return nums
+
+        # INSERTION SORT Trial
+
+        # def insertion_sort(self, A):
+        #     for i in range(1, len(A)):
+        #         j = i
+        #         while j > 0:
+        #             if A[j] > A[j-1]:
+        #                 A[j], A[j-1] = A[j-1], A[j]
+        #                 j -= 1
+        #     return (A)
+        # insertion_sort(self, nums)        
+
+        if len(nums) <= 1:
+            return nums
+        
+        mid = len(nums)//2
+
+        left = nums[:mid]
+        right = nums[mid:]
+
+        self.sortArray(left)
+        self.sortArray(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+        return nums
+
+
+
