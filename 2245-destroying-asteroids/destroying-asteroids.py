85d6af668452214e3ead0726995d40855756da3e
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-09-18 14:58:04 -0400

    Time: 812 ms (75.74%) | Memory: 30.5 MB (50.74%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,5 +1,17 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        # asteroids.sort()
+        # planet_size = mass
+
+        # for asteroid in asteroids:
+        #     if planet_size >= asteroid:
+        #         planet_size += asteroid
+        #     else:
+        #         return False
+        # return True
+
+
+        # SECOND ATTEMPT
         asteroids.sort()
         planet_size = mass
 
