04c5b9f914e4171c10049e547b916bd286e48070
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-09-26 14:22:06 -0400

    Time: 462 ms (65.87%) | Memory: 29.9 MB (18.34%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -22,8 +22,27 @@ class Solution:
         # return select_sort(0, len(nums)-1)
 
         # BRUTE FORCE METHOD
-        nums.sort(reverse=True)
-        print(nums)
+        # You sort it in decreasing order and return the kth largest one (Don't have to worry about the duplicates because they don't want kth distinct)
+        nums.sort(reverse = True)
         return nums[k-1]
+
+        # Without sorting solution (Using quick select)
+        def quickSelect(l,r):
+            pivot = nums[r]
+            p = l
+            for i in range(l,r):
+                if nums[i] <= pivot:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+                nums[i], pivot = pivot, nums[i]
+
+            if p < k:
+                return quickSelect(l,p+1)
+            elif p > k:
+                return quickSelect(p+1, r)
+            else:
+                return nums[p]
+        return quickSelect(0, len(nums)-1)
+
 
         
