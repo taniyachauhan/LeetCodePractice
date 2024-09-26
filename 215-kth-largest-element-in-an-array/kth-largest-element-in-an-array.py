836a47fa369b45b98ab4dc4946a526be31426420
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-09-26 14:24:10 -0400

    Time: 462 ms (65.87%) | Memory: 30 MB (18.34%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,25 +1,5 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # k = len(nums) - k
-
-        # # If we use select sort then average time complexity would be better
-        
-        # def select_sort(l,r):
-        #     pivot, p = nums[len(nums)-1], 0
-        #     for i in range(len(nums)):
-        #         if nums[i] <= pivot:
-        #             nums[i], p = p, nums[i]
-        #             p += 1
-        #         else:
-        #             continue
-        #         nums[p], pivot = pivot, nums[p]
-        # if p == k:
-        #     return nums[p]
-        # elif k > p:
-        #     return select_sort(p,len(nums)-1)
-        # else:
-        #     return select_sort(0,p+1)
-        # return select_sort(0, len(nums)-1)
 
         # BRUTE FORCE METHOD
         # You sort it in decreasing order and return the kth largest one (Don't have to worry about the duplicates because they don't want kth distinct)
