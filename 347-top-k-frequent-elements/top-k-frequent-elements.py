c0dfb866519f4227cab96fe7914bf65185caf454
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-09-27 16:30:42 -0400

    Time: 146 ms (5.01%) | Memory: 21.1 MB (88.12%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -6,7 +6,32 @@ class Solution:
                 map_dict[nums[i]] += 1
             else:
                 map_dict[nums[i]] = 1
-        map_dict=sorted(map_dict.items(), key=lambda item: item[1])
-        return [x[0] for x in map_dict][-k:]
+        # map_dict=sorted(map_dict.items(), key=lambda item: item[1])
+        # return [x[0] for x in map_dict][-k:]
 
+
+
+        keys=[x for x in map_dict.keys()]
+        k = len(keys)-k
+
+        def quickSelect(l, r):
+            
+            pivot = map_dict[keys[r]]
+            p = l
+            for i in range(l,r):
+                if map_dict[keys[i]] <= pivot:
+                    keys[i], keys[p] = keys[p], keys[i]
+                    p += 1
+            keys[p], keys[r] = keys[r], keys[p]
+
+            if p > k:
+                return quickSelect(l, p-1)
+            elif p < k:
+                return quickSelect(p+1, r)
+            else:
+                return p
+        print(keys)
+        print(quickSelect(0, len(keys)-1))
+        partition = quickSelect(0, len(keys)-1)
+        return keys[partition:]
         
