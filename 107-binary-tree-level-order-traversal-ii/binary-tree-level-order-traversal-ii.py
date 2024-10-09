1c92b05208610b0a628ba53eec2b2b64518700b8
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-10-09 19:31:42 -0400

    Time: 34 ms (76.35%) | Memory: 16.8 MB (24.92%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # my_queue = collections.deque()
+        # my_queue.append(root)
+        # result = []
+
+        # while my_queue:
+        #     queue_len = len(my_queue)
+        #     level = []
+        #     for i in range(queue_len):
+        #         node = my_queue.popleft()
+        #         if node:
+        #             level.append(node.val)
+        #             my_queue.append(node.left)
+        #             my_queue.append(node.right)
+        #     if level:
+        #         result.append(level)
+        # return result[::-1]
+
+        # SECOND ATTEMPT
+
+        if not root:
+            return 
+
+        result = []
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+
+            if level:
+                result.append(level)
+        return result[::-1]
+
+        
