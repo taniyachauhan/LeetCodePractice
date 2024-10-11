9ccba2a4826c4797d477bcdf117a57d1f0c4a8f8
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-10-11 16:57:36 -0400

    Time: 37 ms (70.48%) | Memory: 17 MB (43.54%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -7,52 +7,98 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        parent_map = {}
-        stack = []
-        stack.append((root, None))
-        if not root:
-            return
-        while stack:
-            node, parent = stack.pop()
-            parent_map[node] = parent
+        # parent_map = {}
+        # stack = []
+        # stack.append((root, None))
+        # if not root:
+        #     return
+        # while stack:
+        #     node, parent = stack.pop()
+        #     parent_map[node] = parent
         
+        #     if node.left:
+        #         stack.append((node.left, node))
+        #     if node.right:
+        #         stack.append((node.right, node))
+
+
+
+        # queue = collections.deque()
+        # queue.append((target, 0)) # node, level
+        # visited = set()
+        # visited.add(target)
+        # if not root:
+        #     return
+
+        # while queue:
+        #     result=[]
+        #     for i in range(len(queue)):
+        #         node, level = queue.popleft()
+                
+        #         if node.left and node.left not in visited:
+        #             queue.append((node.left, level + 1))
+        #             visited.add(node.left)
+        #         if node.right and node.right not in visited:
+        #             queue.append((node.right, level + 1))
+        #             visited.add(node.right)
+        #         if parent_map[node] != None and parent_map[node] not in visited:
+        #             queue.append((parent_map[node], level + 1))
+        #             visited.add(parent_map[node])
+        #         result.append(node.val)
+
+
+        #     if level == k:
+        #         return result
+        
+        # return []
+
+        # SECOND ATTEMPT
+
+        # Constructing a graph out of this binary tree by traversing this tree in a BFS manner
+
+        graph = collections.defaultdict(list)
+
+        queue = collections.deque()
+        queue.append(root)
+
+        while queue:
+            node = queue.popleft()
+
             if node.left:
-                stack.append((node.left, node))
+                graph[node.val].append(node.left.val)
+                graph[node.left.val].append(node.val)
+
+                queue.append(node.left)
+            
             if node.right:
-                stack.append((node.right, node))
+                graph[node.val].append(node.right.val)
+                graph[node.right.val].append(node.val)
 
-
-
+                queue.append(node.right)
 
 
         queue = collections.deque()
-        queue.append((target, 0)) # node, level
-        visited = set()
-        visited.add(target)
-        if not root:
-            return
+        queue.append((target.val,0))
 
+        visited = set([target.val])
+
+        result = []
         while queue:
-            result=[]
-            for i in range(len(queue)):
-                node, level = queue.popleft()
+            curr_node, distance = queue.popleft()
+            if distance == k:
+                result.append(curr_node)
+            for edge in graph[curr_node]:
+                if edge not in visited:
+                    visited.add(edge)
+                    queue.append((edge, distance+1))
+        return result
+
+
                 
-                if node.left and node.left not in visited:
-                    queue.append((node.left, level + 1))
-                    visited.add(node.left)
-                if node.right and node.right not in visited:
-                    queue.append((node.right, level + 1))
-                    visited.add(node.right)
-                if parent_map[node] != None and parent_map[node] not in visited:
-                    queue.append((parent_map[node], level + 1))
-                    visited.add(parent_map[node])
-                result.append(node.val)
 
 
-            if level == k:
-                return result
-        
-        return []
+
+
  
 
 
