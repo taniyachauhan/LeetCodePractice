d608d406f90f710043abab7073d0b1702f9c744f
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-10-11 14:10:21 -0400

    Time: 71 ms (21.88%) | Memory: 20 MB (94.38%) - LeetSync

--- 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.py
+++ 1883-find-distance-in-a-binary-tree/find-distance-in-a-binary-tree.py
@@ -6,42 +6,46 @@
 #         self.right = right
 class Solution:
     def findDistance(self, root: Optional[TreeNode], p: int, q: int) -> int:
-        parent_map = {}
-        stack = [(root, None)]
+        graph = collections.defaultdict(list)
 
-        while stack:
-            node, parent = stack.pop()
-            if node.left:
-                stack.append((node.left, node))
-            if node.right:
-                stack.append((node.right, node))
-            parent_map[node] = parent
-            if node.val == p:
-                start = node
-            if node.val == q:
-                end = node
-        
-        # Starting with the bfs to find the distance between start and end node
         queue = collections.deque()
-        queue.append((start, 0)) # starting_node, level
-        visited = set()
-        visited.add(start)
+        queue.append(root)
 
         while queue:
-            for i in range(len(queue)):
-                node, level = queue.popleft()
-                if node.left and node.left not in visited:
-                    queue.append((node.left, level + 1 ))
-                    visited.add(node.left)
-                if node.right and node.right not in visited:
-                    queue.append((node.right, level + 1))
-                    visited.add(node.right)
-                if parent_map[node] and parent_map[node] not in visited:
-                    queue.append((parent_map[node], level + 1))
-                    visited.add(parent_map[node])
+            node = queue.popleft()
 
-                if node == end:
-                    return level
+            if node.left:
+                graph[node.val].append(node.left.val)
+                graph[node.left.val].append(node.val)
+
+                queue.append(node.left)
+
+            if node.right:
+                graph[node.val].append(node.right.val)
+                graph[node.right.val].append(node.val)
+
+                queue.append(node.right)
+        
+        # Now trying to find the distance between the nodes using BFS again
+        queue = collections.deque()
+        queue.append([p,0])
+
+        visited = set([p])
+
+        while queue:
+            curr_node, steps = queue.popleft()
+            if curr_node == q:
+                return steps
+            else:
+                for edge in graph[curr_node]:
+                    if edge not in visited:
+                        visited.add(edge)
+                        queue.append((edge, steps+1))
+
+
+
+
+
 
 
 
