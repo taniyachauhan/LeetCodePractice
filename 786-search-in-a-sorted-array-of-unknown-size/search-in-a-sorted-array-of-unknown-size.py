b334c191d0af5cca854bab4bf8a0500d3ef0d118
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2024-10-12 22:52:05 -0400

    Time: 42 ms (50.24%) | Memory: 17.7 MB (63.46%) - LeetSync

---
+++ 786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.py
@@ -0,0 +1,29 @@
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        def binarySearch(self, k):
+            f=0
+            l=i
+            while f<=l:
+                mid=f+(l-f)//2
+                if reader.get(mid)==target:
+                    return mid
+                elif reader.get(mid)< target:
+                    f=mid+1
+                elif reader.get(mid) > target:
+                    l=mid-1
+            return -1
+        i=0
+        while reader.get(i) < target:
+            i = 5+i
+
+        return binarySearch(self, i)
+        
+
+            
