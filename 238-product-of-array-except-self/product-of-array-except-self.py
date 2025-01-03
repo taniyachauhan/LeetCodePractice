02435a756dde1f718aec6eea82a5e8689a41544f
Taniya Chauhan <98198605+taniyachauhan@users.noreply.github.com>
2025-01-03 15:40:06 -0500

    Time: 27 ms (49.11%) | Memory: 23.3 MB (43.87%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         result = [1] * n
 
-        for i in range(1, n):
+        for i in range(1,n):
             result[i] = result[i - 1] * nums[i - 1]
 
         suffix = 1
